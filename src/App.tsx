3b91471bbbf98c87d22be5795b018f37ba3d1710
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-03-15 14:45:50 -0700

    navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/search" element={<Search/>} />
+    <Route path="/watch/:id" element={<Watch/>} />
+   </Routes>
+   </BrowserRouter>
   )
 }
 
